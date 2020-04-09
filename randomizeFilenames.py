38b489b2d62351ed0205d5d2de0361e11c07143f
panchoso2 <fran_rhino@hotmail.com>
2020-04-09 15:54:50 -0400

    Updated to python 3.8
    Script updated to python 3.8 from python 2.7.
    Module Tkinter changed

--- randomizeFilenames.py
+++ randomizeFilenames.py
@@ -1,11 +1,13 @@
+# this script uses python 2.7
+
 import os
 import random
 import string
-import Tkinter
-from Tkinter import *
-from tkCommonDialog import Dialog
-import tkFileDialog
-import tkMessageBox
+import tkinter
+from tkinter import *
+from tkinter.commondialog import Dialog
+import tkinter.filedialog
+import tkinter.messagebox
 
 # generate random name function
 def randomString(stringLength):
@@ -14,7 +16,7 @@ def randomString(stringLength):
 
 
 def directoryBrowser(window, directory):
-    path = tkFileDialog.askdirectory(initialdir="/",title='Please select a directory')
+    path = tkinter.filedialog.askdirectory(initialdir="/",title='Please select a directory')
     directory.set(path)
     return path
 
@@ -29,15 +31,15 @@ def changeFilenames(path):
             newFile = path + "\\" + newName + oldFileExtension
             os.rename(oldFile, newFile)
     else:
-        tkMessageBox.showinfo("Error", "No Directory selected!")
+        tkinter.messagebox.showinfo("Error", "No Directory selected!")
     return TRUE
 
 
 
 # generate window
-window = Tkinter.Tk()
+window = tkinter.Tk()
 window.title("Randomize Filenames")
-window.geometry("500x500")
+window.geometry("250x250")
 
 
 
@@ -50,9 +52,9 @@ selectedDirectoryLabel.pack()
 
 
 # buttons
-buttonSelect = Tkinter.Button(window, text="Select Directory", command=lambda: directoryBrowser(window, directory))
+buttonSelect = tkinter.Button(window, text="Select Directory", command=lambda: directoryBrowser(window, directory))
 buttonSelect.pack()
-buttonStart = Tkinter.Button(window, text="Change Filenames", command=lambda: changeFilenames(directory.get()))
+buttonStart = tkinter.Button(window, text="Change Filenames", command=lambda: changeFilenames(directory.get()))
 buttonStart.pack()
 
 
